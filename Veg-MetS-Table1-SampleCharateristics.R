95a65535f6016f3ff846dcf972fb1171141131ff
Stephane SMbatchou <smbatchou@ip-10-118-67-38.ec2.internal>
2015-03-25 10:45:34 -0400

    'Continuous variables characteristics'

--- Veg-MetS-Table1-SampleCharateristics.R
+++ Veg-MetS-Table1-SampleCharateristics.R
@@ -1,8 +1,9 @@
+rm(list=ls());gc()
 #loading important (or required) packages
 
 library(opal)                         
 library(dsBaseClient)                 
-library(dsStatsClient)  	     
+library(dsStatsClient)         
 library(dsModellingClient)
 library(datashieldclient)
 
@@ -24,42 +25,64 @@ ld<-subset(logindata, server %in% study)
 opals <- datashield.login(logins=ld,assign=TRUE,variables=myvar)
 
 #verify the validity of the whole dataframe 'D'
+message('\n\nTesting Data validity for all studies...')
 suppressWarnings(all(ds.isValid('D')))
-
+message('VALIDITY TESTING DONE')
 ############################################
 #   REORGANIZE DATA BY VEGETARIAN (yes/no)
 # SUBCLASS AND ASSIGN TO D_VEG_0 AND D_VEG_1
-ds.subclass('D','D_VEG','DIET_VEGETARIAN')
-ds.assign('D_VEG$DIET_VEGETARIAN.level_1','D_VEG_1')
-ds.assign('D_VEG$DIET_VEGETARIAN.level_0','D_VEG_0')
+sub_by<-'DIET_VEGETARIAN'                       #<=== may vary often 
+message(paste0('\n\n==> SUBSETTING DATA BY '),sub_by,'\nWait do not interrupt!...')
+ds.subclass('D','D_VEG',sub_by)
+D_sub_name_1<-paste0('D_VEG$',sub_by,'.level_1')
+D_sub_name_2<-paste0('D_VEG$',sub_by,'.level_0')
+ds.assign(D_sub_name_1,'D_VEG_1')
+ds.assign(D_sub_name_2,'D_VEG_0')
+message(paste0('==> SUBSETTING DATA BY '),sub_by,' IS OK\n')
 
 
+######################################################
+#
+#         ******CONTINUOUS VARIABLES STATISTICS **** 
+#
+######################################################
+var_cont<-list( 'AGE_YRS','PM_WAIST_SIZE','PM_SYSTOLIC_MEASURE', 
+                'PM_DIASTOLIC_MEASURE', 'LAB_GLUC_FASTING','LAB_HDL', 'LAB_TRIG','LAB_hsCRP')
 
-############age mean by vegetarian for each study##################
-ds.isValid('D$AGE_YRS')
-age_by_veg_split<-ds.meanByClass('D','AGE_YRS','DIET_VEGETARIAN',type='split')
-##compute the corresponding t-test using ds.glm for AGE_YRS~DIET_VEGETARIAN
-glm_age_vs_veget_split<-lapply(opals,function(op){ds.glm(D$AGE_YRS~D$DIET_VEGETARIAN,'gaussian',datasources=list(op))})
-##compute the t.test using data from D_VEG_XXX
-ttest_age_by_Veg_split<-ds.tTest('D_VEG_1$AGE_YRS','D_VEG_0$AGE_YRS',paired=F,type='split')
-pvalue_age_by_veg_split<-lapply(ttest_age_by_Veg_split,function(x){x$p.value})
+####******BY STUDY: SPLIT
+#mean by veget.
+total <- length(var_cont)
+# create progress bar
+pb <- txtProgressBar(min = 0, max = total, style = 3)
+i<-1
+result_cont<-NULL
+for (var in var_cont){
+  
+  #compute mean by class
+  message(paste0('\n==> COMPUTING MEAN_VALUE FOR ',var,'~',sub_by))
+  mean_split<-ds.meanByClass('D',var,sub_by,type='split')
+  message(paste0('==> MEAN_VALUE FOR ',var,'~',sub_by,' IS OK'))
+  
+  #compute the t.test using data from D_VEG_XXX
+  message(paste0('\n==> COMPUTING Ttest P_VALUE FOR ',var,'~',sub_by,'\nDo not interrupt!...'))
+  var1<-paste0('D_VEG_1$',var)
+  var2<-paste0('D_VEG_0$',var)
+  ttest_split<-ds.tTest(var1,var2,paired=F,type='split')
+  pvalue_split<-lapply(ttest_split,function(x){x$p.value})
+  message(paste0('==>P_VALUE FOR ',var,'~',sub_by,' IS OK'))
+  
+  #arranging final result
+  z<-list(pvalue = pvalue_split, mean_stats = mean_split)
+  w<-structure(list(z),.Names=var)
+  result_cont<-c(result_cont,w)
+  setTxtProgressBar(pb, i)
+  i<-i+1
+}
+close(pb)
 
-
-#####age mean by vegetarian for the combined valid studies############
-age_by_veg_comb<-ds.meanByClass('D','AGE_YRS','DIET_VEGETARIAN')
-age_by_veg_comb<-data.frame(age_by_veg_comb,stringsAsFactors=F)
-##compute the corresponding t-test using ds.glm for AGE_YRS~DIET_VEGETARIAN
-glm_age_vs_veget_comb<-ds.glm(D$AGE_YRS~D$DIET_VEGETARIAN,'gaussian')
-##compute the t.test using data from D_VEG_XXX
-ttest_age_by_Veg_comb<-ds.tTest('D_VEG_1$AGE_YRS','D_VEG_0$AGE_YRS',paired=F)
-pvalue_age_by_veg_comb<-as.numeric(ttest_age_by_Veg_comb$p.value)
-
-file_name<-paste0('RESULTS_ver::',Sys.Date(),'.Rdata')
-save(age_by_veg_comb,age_by_veg_split,pvalue_age_by_veg_comb,pvalue_age_by_veg_split,ttest_age_by_Veg_comb,ttest_age_by_Veg_split,file=file_name)
-
-
-
-
-
+##Saving Result object in file versioned
+result_cont_name<-paste0('RESULTS_Cont_',sub_by,'_::',Sys.Date(),'.Rdata')
+message(paste0('\n\n******\t\tSaving Results for Continous Variables in <',result_cont_name,'> file.'))
+save(result_cont,file=result_cont_name)
 
 datashield.logout(opals)
