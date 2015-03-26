d7f69ea5ccfbc527e01365920cbfb690cdbe6da7
Dany Doiron <ddoiron@maelstrom-research.org>
2015-03-26 17:04:53 -0400

    new edu variable

--- Veg-MetS-Table1-SampleCharateristics.R
+++ Veg-MetS-Table1-SampleCharateristics.R
@@ -8,7 +8,7 @@ library(dsModellingClient)
 library(datashieldclient)
 
 #the variables needed for the HOP 
-myvar <- list('DIET_VEGETARIAN', 'DIET_VEGETARIAN_VERIFIED', 'AGE_YRS', 'AGE_YRS_CATEGORICAL', 'GENDER', 'EDU_HIGHEST_1', 
+myvar <- list('DIET_VEGETARIAN', 'DIET_VEGETARIAN_VERIFIED', 'AGE_YRS', 'AGE_YRS_CATEGORICAL', 'GENDER', 'EDU_HIGHEST_2', 
               'WORK_STATUS_CURRENT', 'SMK_CIG_CURRENT', 'ALC_CURRENT', 'PM_BMI_CATEGORIAL', 'PM_WAIST_SIZE', 
               'PM_SYSTOLIC_MEASURE', 'PM_DIASTOLIC_MEASURE', 'LAB_GLUC_FASTING', 'LAB_HDL', 'LAB_TRIG', 'LAB_hsCRP', 
               'METABSYNDR_NBR_STRICT', 'METABSYNDR_STRICT', 'METABSYNDR_NBR_MODERATE', 'METABSYNDR_MODERATE')
@@ -18,7 +18,7 @@ load("~datashield/hop/logindata.hop.rda")
 
 #only studies participating in MetS and Vegetarian diet:  finrisk, kora, lifelines, mitchelstown, cartagene
 #finrisk and cartagene are not ready yet
-study<-c('kora','mitchelstown','lifelines','finrisk')
+study<-c('mitchelstown','lifelines','finrisk')#('kora'
 ld<-subset(logindata, server %in% study)
 
 #login to datashield and assign data to 'D'
@@ -31,7 +31,7 @@ message('VALIDITY TESTING DONE')
 
 
 #Define variable to subset by
-sub_by<-'DIET_VEGETARIAN'                       #<=== may vary often 
+sub_by<-'DIET_VEGETARIAN_VERIFIED'                       #<=== may vary often 
 
 
 ######################################################
@@ -43,7 +43,7 @@ var_cont<-list( 'AGE_YRS','PM_WAIST_SIZE','PM_SYSTOLIC_MEASURE',
                 'PM_DIASTOLIC_MEASURE', 'LAB_GLUC_FASTING','LAB_HDL', 'LAB_TRIG','LAB_hsCRP')
 
 #####STATS WITH SPLIT#########
-source('continuous_var_stats_split.R',echo=F,print.eval=T)
+#source('continuous_var_stats_split.R',echo=F,print.eval=T)
 
 
 ######################################################
@@ -59,5 +59,6 @@ var_cat<-list('AGE_YRS_CATEGORICAL','GENDER','SMK_CIG_CURRENT', 'ALC_CURRENT', '
 source('categorical_var_stats_split.R',echo=F,print.eval=T)
 
 
-rm(list=ls())
+
 datashield.logout(opals)
+rm(list=ls())
