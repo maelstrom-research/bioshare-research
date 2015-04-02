e00656dddeeb09bab14afcd04c2f71c260c7ba73
smbatchou <stephabio@yahoo.ca>
2015-04-02 12:01:27 -0400

    'update the new ttest for continuous vars'

--- continuous_var_stats_split.R
+++ continuous_var_stats_split.R
@@ -9,22 +9,8 @@ message('--CONTINUOUS VARIABLES SELECTED: ')
 cat(unlist(var_cont))
 
 
-############################################
-#   REORGANIZE DATA BY VEGETARIAN (yes/no)
-# SUBCLASS AND ASSIGN TO D_VEG_0 AND D_VEG_1
-
-message(paste0('\n\n==> SUBSETTING DATA BY '),sub_by,'\nWait do not interrupt!...')
-ds.subclass('D','D_VEG',sub_by)
-D_sub_name_1<-paste0('D_VEG$',sub_by,'.level_1')
-D_sub_name_2<-paste0('D_VEG$',sub_by,'.level_0')
-ds.assign(D_sub_name_1,'D_VEG_1')
-ds.assign(D_sub_name_2,'D_VEG_0')
-message(paste0('==> SUBSETTING DATA BY '),sub_by,' IS OK\n')
-
-
 
 ####******BY STUDY: SPLIT
-#mean by veget.
 
 ###preparing progress bar
 total <- length(var_cont)
@@ -35,16 +21,16 @@ i<-1
 result_cont_split<-NULL
 for (var in var_cont){
   
+  #define formula
+  regformula<-paste0('D$',var,'~D$',sub_by)
   #compute mean by class
   message(paste0('\n==> COMPUTING MEAN_VALUE FOR ',var,'~',sub_by))
-  mean_split<-ds.meanByClass('D',var,sub_by,type='split')
+  mean_split<-ds.meanByClass(regformula,type='split')
   message(paste0('==> MEAN_VALUE FOR ',var,'~',sub_by,' IS OK'))
   
   #compute the t.test using data from D_VEG_XXX
-  message(paste0('\n==> COMPUTING Ttest P_VALUE FOR ',var,'~',sub_by,'\nDo not interrupt!...'))
-  var1<-paste0('D_VEG_1$',var)
-  var2<-paste0('D_VEG_0$',var)
-  ttest_split<-ds.tTest(var1,var2,paired=F,type='split')
+  message(paste0('\n==> COMPUTING ttest P_VALUE FOR ',var,'~',sub_by,'\nDo not interrupt!...'))
+  ttest_split<-ds.tTest(regformula,paired=F,type='split')
   pvalue_split<-lapply(ttest_split,function(x){x$p.value})
   message(paste0('==>P_VALUE FOR ',var,'~',sub_by,' IS OK'))
   
