508c7d7fede2e62bb58f96347b5267f457189837
Dany Doiron <ddoiron@maelstrom-research.org>
2015-04-02 10:35:34 -0400

    added EDU_HIGHEST_1 , excluded KORA, and sub_by DIET_VEGETARIAN_VERIFIED

--- Veg-MetS-Table1-SampleCharateristics.R
+++ Veg-MetS-Table1-SampleCharateristics.R
@@ -8,7 +8,7 @@ library(dsModellingClient)
 library(datashieldclient)
 
 #the variables needed for the HOP 
-myvar <- list('DIET_VEGETARIAN', 'DIET_VEGETARIAN_VERIFIED', 'AGE_YRS', 'AGE_YRS_CATEGORICAL', 'GENDER', 'EDU_HIGHEST_2', 
+myvar <- list('DIET_VEGETARIAN', 'DIET_VEGETARIAN_VERIFIED', 'AGE_YRS', 'AGE_YRS_CATEGORICAL', 'GENDER', 'EDU_HIGHEST_1', 'EDU_HIGHEST_2',
               'WORK_STATUS_CURRENT', 'SMK_CIG_CURRENT', 'ALC_CURRENT', 'PM_BMI_CATEGORIAL', 'PM_WAIST_SIZE', 
               'PM_SYSTOLIC_MEASURE', 'PM_DIASTOLIC_MEASURE', 'LAB_GLUC_FASTING', 'LAB_HDL', 'LAB_TRIG', 'LAB_hsCRP', 
               'METABSYNDR_NBR_STRICT', 'METABSYNDR_STRICT', 'METABSYNDR_NBR_MODERATE', 'METABSYNDR_MODERATE')
@@ -18,7 +18,7 @@ load("~datashield/hop/logindata.hop.rda")
 
 #only studies participating in MetS and Vegetarian diet:  finrisk, kora, lifelines, mitchelstown, cartagene
 #finrisk and cartagene are not ready yet
-study<-c('mitchelstown','lifelines','finrisk','kora')
+study<-c('mitchelstown','lifelines','finrisk') #,'kora'
 ld<-subset(logindata, server %in% study)
 
 #login to datashield and assign data to 'D'
@@ -31,7 +31,7 @@ message('VALIDITY TESTING DONE')
 
 
 #Define variable to subset by
-sub_by<-'DIET_VEGETARIAN'                       #<=== may vary often: ALWAYS CALL THIS VARIABLE 
+sub_by<-'DIET_VEGETARIAN_VERIFIED'                       #<=== may vary often: ALWAYS CALL THIS VARIABLE 
 
 
 ##########################################################################
@@ -59,7 +59,7 @@ source('continuous_var_stats_combined.R',echo=F,print.eval=T)
 ########################################################################
 
 var_cat<-list('AGE_YRS_CATEGORICAL','GENDER','SMK_CIG_CURRENT', 'ALC_CURRENT', 'PM_BMI_CATEGORIAL',
-              'METABSYNDR_NBR_STRICT','METABSYNDR_STRICT','METABSYNDR_NBR_MODERATE','METABSYNDR_MODERATE','EDU_HIGHEST_2') #,'WORK_STATUS_CURRENT'
+              'METABSYNDR_NBR_STRICT','METABSYNDR_STRICT','METABSYNDR_NBR_MODERATE','METABSYNDR_MODERATE', 'EDU_HIGHEST_1', 'EDU_HIGHEST_2') #,'WORK_STATUS_CURRENT'
 
 #################
 #STATS WITH SPLIT
