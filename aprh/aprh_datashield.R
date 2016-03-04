184f3f422dbef6cda2ee6eda89f248ee29e2be05
smbatchou <stephabio@yahoo.ca>
2016-03-04 18:13:22 -0500

    organize file

---
+++ aprh/aprh_datashield.R
@@ -0,0 +1,95 @@
+################################################################
+#
+#                         MAIN ANALYSIS
+#
+###############################################################
+
+# Attach the DS UTILS environnment
+source('../bioshare-research/utils/utils_analysis.R',echo=F,print.eval=F)
+
+# LOAD DATAs IN EACH OPAL SERVER
+source('aprh_data.R',echo=F,print.eval=F)
+
+
+########################     DATA THAT WILL BE USED FOR THE MAIN ANALYSIS      ############
+#specify the name (character) of the dataframe that was assigned in datashield.login ('D' by default)
+data <- 'D'
+
+#################################################################################
+##            running some regressions :multivariate                           ##
+#################################################################################
+
+####################### define models [model0,model1, model2a,model2b model3a,model3b]
+### model0 (unajusted)
+model <- NULL
+
+###model_1(ajusted for age,sex )                                                                                     <-
+model<-'AGE_YRS+GENDER'
+
+#Model 2a (adjusted for age, sex, bmi, highest level of education)                        <-
+model<-'AGE_YRS+GENDER+EDU_HIGHEST_2+PM_BMI_CATEGORIAL'
+
+#Model 2b (adjusted for age, sex, bmi, highest level of education and household income)
+model <- 'AGE_YRS+GENDER+EDU_HIGHEST_2+PM_BMI_CATEGORIAL+INCOME'
+
+# Model 3a: adjusted for age, sex, bmi, highest level of education, smoking status, and exposure to second-hand tobacco smoke  				
+model <- 'AGE_YRS+GENDER+PM_BMI_CATEGORIAL+EDU_HIGHEST_2+SMK_STATUS+SMK_PASSIVE_ALL'
+
+#Model 3b: adjusted for age, sex, bmi, highest level of education, household income, smoking status, and exposure to second-hand tobacco smoke 
+model <- 'AGE_YRS+GENDER+PM_BMI_CATEGORIAL+EDU_HIGHEST_2+SMK_STATUS+SMK_PASSIVE_ALL+INCOME'
+
+
+
+#######################
+expo <- 'PM25_ESCAPE'
+#expo <- 'PM10_ESC'
+#expo <- 'NO2_ESCAPE'
+#expo <- 'PMcoarse_ESCAPE'
+
+####################
+outcome <- 'SYM_WHEEZ' 
+#outcome <- 'SYM_WHEEZ_NOCOLD'
+#outcome <- 'SYM_SBREATH'
+#outcome <- 'SYM_SBREATH_WALK'
+#outcome <- 'SYM_BREATH_PRB'
+#outcome <- 'SYM_PHLEGM_UP'
+#outcome <- 'SYM_PHLEGM_UP_FREQ'
+#outcome <- 'SYM_PHLEGM_DAY'
+#outcome <- 'SYM_PHLEGM_DAY_FREQ'
+#outcome <- 'SYM_SBREATH_EXT'
+
+#pooled model to run [first create dummy study variables in server side by running run.dummy() once]
+run.dummy.study(data) #run once 
+datashield.symbols(opals) # check objects on server side
+
+#exposure effect statistics  [see data = 'D' for main analysis]
+run.model(outcome,expo,model,family = 'binomial',data,Ncases=T,pval = F, ref ='lifelines',check=F) #ref is specified here
+
+#split model run [ no need to create dummy study variable, but you need to specify the datasource] 
+run.model(outcome,expo,model,family = 'binomial',data,Ncases=T, pval = F, datasources = opals[2],check=F) #ukbiobank opals[2]
+
+
+
+### ------generate formula based on previously called expo, outcome, model and data.
+#formula <- run.update.formula(outcome,expo,model,data) #[ data = 'D' main analysis]
+#glm.res<-run.meta.glm(formula,family='binomial',ref='lifelines')
+#run.extract.glm.stats(glm.res)
+
+
+################# NA STATS ##########################
+
+#### create NA subset ex: NA.D (default for main analysis and for a specific model) on server side
+#this function assign NA.D.xxxx on server side and return the name of the assign NA dataframe.
+nadata <- run.NA.glm.subset(formula=formula)
+
+#check number of missing cases
+ds.dim(nadata)
+
+#### run NA stats 
+run.desc.stats('AGE_YRS',data = nadata)
+run.desc.stats('GENDER',data= nadata)
+run.desc.stats('PM_BMI_CATEGORIAL',data= nadata)
+run.desc.stats('EDU_HIGHEST_2',data= nadata)
+
+
+
---
+++ utils/graphics_utils.R
@@ -0,0 +1,144 @@
+# Source the DS UTILS environnment
+source('utils_analysis.R',echo=F,print.eval=F)
+#forestplot library
+library('forestplot',quietly=T)
+
+#.glist function will run many glms and return a list of all the glm compute based on the by params
+#ex: if by = 'expo', .glist will compute a list of glm by 'expo'
+#param <X> = the list of names (charcter) to compute by
+#param <expo> = the expo var (in character) 
+#param <outcome> = the outcome var (in character)
+#param <model> = the model object
+#param <data> = the data where the variables the reside
+#param <fam> = family of the glm
+#param <ref> = the study selected as reference in a heterogeneity model
+#param <by> = the name in charactr of the option to compute the list. 
+#ex: <by> can be either 'expo' to compute the list of glm by expo var 
+#,this implies that <X> is a vector or a list of expo var names.
+#<by> can be either 'expo','outcome', or 'model'
+# EX: 
+#expo.c <- c('expo1','expo2','expo3')
+#gstack <- .glist.by(expo.c,outcome=outcome,model=model,data='D',fam='binomial',by='expo',datasources=opals[1])
+
+.glist.by <- function(expo,outcome,model,data,fam,ref,by,datasources,...)
+{
+  if(missing(by)) {stop('[by] is mandatory',call.=F)}
+  else{info <- by}
+  
+  if(missing(datasources)){
+    datasources<-findLoginObjects()
+  }
+  ds <- datasources
+  
+  .gl <- function(x) {
+    if(grepl('expo',info,T)) {expo <- x}
+    else if(grepl('outcome',info,T)) {outcome <- x}
+    else if (grepl('model',info,T)) {model <- x}
+    else {stop('[by] should be either "expo","outcome" or "model"',call.=F)}
+    formul <- run.make.formula(outcome,expo,model,data)
+    run.meta.glm(formul,family=fam,ref=ref,datasources = ds,...) 
+  }
+  
+  if(grepl('expo',info,T)) {
+    result <- sapply(expo,.gl,simplify = FALSE, USE.NAMES = TRUE)
+  }else if(grepl('outcome',info,T)) {
+    result <- sapply(outcome,.gl,simplify = FALSE, USE.NAMES = TRUE)
+  }
+  else if (grepl('model',info,T)) {
+    result <- sapply(model,.gl,simplify = FALSE, USE.NAMES = TRUE)
+  }else {
+    stop('[by] should be either "expo","outcome" or "model"',call.=F)
+  }
+  return (result)
+}
+
+#.fp.info function take one ds glm result and return a dataframe with mean lower upper of the 
+# estimated of expo variable. 
+#This dataframe will be used by forestplot function
+#By default the function assumes that the expo var is the last term in the glm formula.
+#Otherwise you can specify the term (variable name in character) of interest
+
+.fp.info <- function(glm.result,term=NULL)
+{
+  if(missing(glm.result)) stop('Please provide a valid glm result...',call.=F)
+  glm.family <- glm.result$family$family
+  glm.coef <- glm.result$coef
+  if(grepl("poisson|binomial", glm.family)){
+    num.cols <- dim(glm.coef)[2]
+    m.ci <- as.data.frame(glm.coef[,(num.cols-2):num.cols],stringsAsFactors=F)
+  }else if (glm.family == 'gaussian'){
+    m.ci <- as.data.frame(glm.coef[,-c(2:4)],stringsAsFactors=F)
+  } 
+  # return the mean estimate or OR or RR and the confidence interval (ci)
+  names(m.ci) <- c('mean','lower','upper')
+  if(is.null(term)) return(m.ci[nrow(m.ci),])
+  return(m.ci[term,])
+  
+}
+
+#.stack.fp.info use a list of glm returned by .glist.by, process and stack them using .fp.info
+# It will return a dataframe ready for forestplot
+
+.stack.fp <- function(glm.list,fix,...){                      # <- TODO TODO TODO  ADD NAMES INFOS IN FUNCTION: FIND BEST WAY TO DO THAT 
+  if(missing(glm.list)) stop('Please provide a valid glm result(s) list...',call.=F)
+  glist <- glm.list
+  
+  if(missing(fix)) stop('Provide a fix name ...',call.=F)
+  label <- paste0('__',fix,'__')
+  k.list <- lapply(glist,.fp.info,...)
+  
+  k.stack <- rbind(c(NA,NA),Reduce(rbind,k.list))
+  row.names(k.stack) <-c(fix, names(glist))
+  
+  k.stack
+}
+
+.label.fp <- function(glm.list,digit=NULL,fix,term = NULL){
+  if(missing(glm.list)) stop('Please provide a valid glm result(s) list...',call.=F)
+  glist <- glm.list
+  
+  space <-'           '
+  
+  if(missing(fix)) stop('Provide a fix name ...',call.=F)
+  fix <- paste0(fix,space)
+  
+  if(is.null(digit)){digit <- 3}
+  
+  
+  #cnames <- names(run.extract.glm.stats(glist[[1]])$stats)
+  f<-function(x){
+    res.all <- run.extract.glm.stats(x,rdigit=digit)$stats
+    #expo <- row.names(res.all)[nrow(res.all)]
+    #cbind(expo,res.all[nrow(res.all),])
+    term <- if(is.null(term)){nrow(res.all)}else{term}
+    res.all[term,]
+  }
+  s.list <- lapply(glist,f)
+  estim <-  Reduce(rbind,s.list)
+  labels<- paste0(space,.checklbl(names(glist),literal=.outcomes.lit))
+  s.stack <- cbind(labels, estim)
+  rbind(c(fix,''),s.stack)
+}
+
+
+.checklbl <- function(lbl,literal){
+  f <- function(x,l){l[[x]]}
+  sapply(lbl,f,literal)
+}
+
+
+
+.outcomes.lit <- c(
+   'SYM_BREATH_PRB' = 'General breathing problems' ,
+   'SYM_WHEEZ' = 'Wheeze', 
+   'SYM_WHEEZ_NOCOLD' = 'Wheeze without a cold' ,
+   'SYM_SBREATH' = 'Shortness of breath at rest',
+   'SYM_SBREATH_WALK' = 'Shortness of breath walking',
+   'SYM_PHLEGM_UP' = 'Phlegm when waking up',
+   'SYM_PHLEGM_UP_FREQ' = 'Phlegm when waking up (almost daily)' ,
+   'SYM_PHLEGM_DAY' = 'Phlegm during the day',
+   'SYM_PHLEGM_DAY_FREQ' = 'Phlegm during the day (almost daily)' ,
+   'SYM_SBREATH_EXT' = 'Shortness of breath (extended definition)'
+)
+
+
