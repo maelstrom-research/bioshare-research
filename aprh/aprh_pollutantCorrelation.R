68fa8158e6fe31c98cacea78260da6ad78147e94
Dany Doiron <ddoiron@maelstrom-research.org>
2016-09-12 19:26:12 +0000

    pollutant correlation and descriptive stats

--- aprh/aprh_pollutantCorrelation.R
+++ aprh/aprh_pollutantCorrelation.R
@@ -28,5 +28,48 @@ ld<-subset(logindata, server %in% study)
 opals <- datashield.login(logins=ld,assign=TRUE,variables=myvar)
 
 
-#build correlation matrix
+#Study-specific quantiles and means
+ds.summary(x='D$PM25_ESCAPE')
+ds.summary(x='D$PM10_ESC')
+ds.summary(x='D$PMcoarse_ESCAPE')
+ds.summary(x='D$NO2_ESCAPE')
+
+#Study-specific standard deviation
+ds.var(x='D$PM25_ESCAPE', type='split')
+sqrt(0.1245475)
+sqrt(1.117355)
+ds.var(x='D$PM10_ESC', type='split')
+sqrt(0.4286679)
+sqrt(3.606458)
+ds.var(x='D$PMcoarse_ESCAPE', type='split')
+sqrt(0.19376)
+sqrt(0.8111445)
+ds.var(x='D$NO2_ESCAPE', type='split')
+sqrt(13.70513)
+sqrt(57.3971)
+
+#Pooled quantiles and mean
+ds.quantileMean(x='D$PM25_ESCAPE')
+ds.quantileMean(x='D$PM10_ESC')
+ds.quantileMean(x='D$PMcoarse_ESCAPE')
+ds.quantileMean(x='D$NO2_ESCAPE')
+
+#Pooled standard deviation
+ds.var(x='D$PM25_ESCAPE')
+sqrt(0.987823)
+ds.var(x='D$PM10_ESC')
+sqrt(3.19185)
+ds.var(x='D$PMcoarse_ESCAPE')
+sqrt(0.730594)
+ds.var(x='D$NO2_ESCAPE')
+sqrt(52.03927)
+
+####CORRELATION MATRICES########
+#Study-specific correlation matrix
 ds.cor(x='D')
+
+#Pooled correlation matrix
+
+
+
+
