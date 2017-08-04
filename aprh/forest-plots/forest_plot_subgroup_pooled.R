8da19d6fb2b4147a8cb0dcebb9c4851c0c008e4e
Dany Doiron <ddoiron@maelstrom-research.org>
2017-08-04 17:24:15 +0000

    can now determine png resolution, width and height

--- aprh/forest-plots/forest_plot_subgroup_pooled.R
+++ aprh/forest-plots/forest_plot_subgroup_pooled.R
@@ -1,7 +1,7 @@
 source('utils/graphics_utils.R')
 
 #read formatted csv file
-data<- read.csv('aprh/forest-plots/PMcoarse_Pooled.csv',header=FALSE,colClasses = 'character') 
+data<- read.csv('aprh/forest-plots/PooledNO2.csv',header=FALSE,colClasses = 'character') 
 csvtext <- .csv_to_text(data)
 
 MLU1 <- .mean_lower_upper(data[1:19,]) 
@@ -9,6 +9,8 @@ MLU2 <- .mean_lower_upper(data[-c(1:19),])
 
 is.summary <- is.na(MLU1[,'mean'])
 title <- 'TEST'
+
+png(file="aprh/forest-plots/PooledNO2.png",width=3600,height=3000,res=300)
 
 forestplot(csvtext, new_page = T,
            is.summary = is.summary,
@@ -18,23 +20,27 @@ forestplot(csvtext, new_page = T,
            graph.pos=3,
            zero = 1,
            fn.ci_norm = c(fpDrawNormalCI, fpDrawCircleCI),
-           txt_gp = fpTxtGp(label = list(gpar(cex=.70),
-                                         gpar(cex=.70),
-                                         gpar(cex=.70),
-                                         gpar(cex=.70, col="blue")),
-                            xlab = gpar(cex = .75),
-                            ticks = gpar(cex = .75),
-                            title = gpar(cex = .75)),
-           hrzl_lines=list("7" = gpar(lwd=76, lineend="butt", columns=c(2:5), col="#99999922"),
-                           "13" = gpar(lwd=76, lineend="butt", columns=c(2:5), col="#99999922"),
-                           "19" = gpar(lwd=76, lineend="butt", columns=c(2:5), col="#99999922")),
-           boxsize = .13, # We set the box size to better visualize the type
-           line.margin =.26 , # We need to add this to avoid crowding
+           txt_gp = fpTxtGp(label = list(gpar(cex=1.05),
+                                         gpar(cex=1.05),
+                                         gpar(cex=1.05),
+                                         gpar(cex=1.05, col="blue")),
+                            legend = gpar(cex =1.05),
+                            xlab = gpar(cex = 1.15),
+                            ticks = gpar(cex = 1.05),
+                            title = gpar(cex = 1.15)),
+           hrzl_lines=list("7" = gpar(lwd=98, lineend="butt", columns=c(2:5), col="#99999922"),
+                           "13" = gpar(lwd=98, lineend="butt", columns=c(2:5), col="#99999922"),
+                           "19" = gpar(lwd=98, lineend="butt", columns=c(2:5), col="#99999922")),
+           boxsize = .15, # We set the box size to better visualize the type
+           line.margin =.20 , # We need to add this to avoid crowding
            mean = cbind(MLU1[,'mean'], MLU2[,'mean']),
            lower = cbind(MLU1[,'lower'], MLU2[,'lower']),
            upper = cbind(MLU1[,'upper'], MLU2[,'upper']),
            #clip = c(.77,1.31),
-           xticks =seq(from=.80,to=1.20,by=.10),  
+           #xticks =seq(from=0.9,to=2.1,by=.1),  
+           xticks =c(from=1,1.5,2),  
            col=fpColors(box=c("blue", "darkred")),
-           xlab="Odds ratio"
+           xlab="OR (95% CI)"
 )
+
+dev.off()
